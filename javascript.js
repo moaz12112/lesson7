1f13da0435455eb8ea389a7429e779d051dbe94a
moaz12112 <140605588+moaz12112@users.noreply.github.com>
2023-08-12 15:37:02 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,53 @@
+/*let msg="<h1> heeeeeey</h1>"
+alert(msg);
+document.writeln("helloworld");
+document.getElementById("my contain").innerHTML =msg;
+console.log(msg);
+*/
+/*let elen = document.getElementById("head");
+alert(elen);
+document.writeln(elen);
+//document.getElementById("can").innerHTML =elen;
+console.log(elen);
+
+{
+    var x=15;
+}
+document.getElementById("can").innerHTML =x;
+*/
+/*let x=6;
+let y=7;
+let s= "hello";
+
+let z=x**y;
+*/
+/*let z=parseInt(prompt("hello   ....."));
+let z=y*2;
+*/
+/*let x=5;
+let y=4;
+let z=x*y;
+*/
+//let i=5;
+/*while(i<=10){
+
+    document.getElementById("contain").innerHTML +=i + "<br>";
+
+    i++
+}
+if(i==11){
+    document.getElementById("contain").innerHTML +=i*2 + "<br>";
+
+}
+*/
+let i=5;
+let z="";
+for(i=0; i<=10; i++){
+    z+=i + "<br>"
+}
+document.getElementById("contain").innerHTML +=z;
+
+
+
+
+
